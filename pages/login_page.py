bfa0c0cdf64f1b8b394cb8ecf36699dee9d9e149
Romil Sakariya <RSakariya@zarion.com>
2025-07-01 10:58:16 +0100

    Adding logging and reporting utilities

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,12 @@
+from utils.logger import get_logger
+logger = get_logger(__name__)
+
 class LoginPage:
     def __init__(self, page):
         self.page = page
 
     def login(self, username, password):
+        logger.info("Logging into application")
         self.page.fill("#user-name", username)
         self.page.fill("#password", password)
         self.page.click("#login-button")
---
+++ utils/logger.py
@@ -0,0 +1,14 @@
+# utils/logger.py
+import logging
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
